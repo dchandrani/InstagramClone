9a788cdc73180f58a18813899b8ab43b3fe61877
Hardik Patel <hardikghanshyampatel@gmail.com>
2020-12-27 18:10:17 +0530

    adding validation

--- app/src/main/java/com/dh/instagramclone/ui/login/LoginActivity.kt
+++ app/src/main/java/com/dh/instagramclone/ui/login/LoginActivity.kt
@@ -1,19 +1,42 @@
 package com.dh.instagramclone.ui.login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import com.dh.instagramclone.R
 import com.dh.instagramclone.databinding.ActivityLoginBinding
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.coroutines.flow.collect
 
 class LoginActivity : AppCompatActivity() {
+
+    private val TAG = "LoginActivity"
 
     private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        setContentView(R.layout.activity_login)
         val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.apply {
+            btnLogin.setOnClickListener {
+                viewModel.loginClicked(edtUserName.text.toString(), edtPassword.text.toString())
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.loginEvent.collect { event ->
+                when(event){
+                    is LoginViewModel.LogInEvent.ShowInvalidMessage -> {
+                        Snackbar.make(binding.root, event.message, Snackbar.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/dh/instagramclone/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/dh/instagramclone/ui/login/LoginViewModel.kt
@@ -4,8 +4,34 @@ import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
 
 
 class LoginViewModel @ViewModelInject constructor(@Assisted private val state: SavedStateHandle) :
     ViewModel() {
+
+    private val loginChannel = Channel<LogInEvent>()
+    val loginEvent = loginChannel.receiveAsFlow()
+
+    fun loginClicked(userName: String, password: String) {
+        if (userName.isBlank()) {
+            showInvalidInputMessage("Please enter valid Username")
+            return
+        }
+        if (password.isBlank()) {
+            showInvalidInputMessage("Password can not be empty")
+            return
+        }
+    }
+
+    private fun showInvalidInputMessage(message: String) = viewModelScope.launch {
+        loginChannel.send(LogInEvent.ShowInvalidMessage(message))
+    }
+
+    sealed class LogInEvent {
+        data class ShowInvalidMessage(val message: String) : LogInEvent()
+    }
 }
